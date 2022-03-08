e35797522fa738e8afe742cfd342750602de9f84
Douglas Moura <DouglasdeMoura@users.noreply.github.com>
2022-03-08 15:55:03 +0000

    chore: improve tests even more

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -93,16 +93,26 @@ suite('Functional Tests with Zombie.js', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
       browser.fill('surname', 'Colombo')
-      browser.pressButton('submit')
-      
-      done();
+      browser.pressButton('submit', () => {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Cristoforo');
+        browser.assert.text('span#surname', 'Colombo');
+        browser.assert.element('span#dates', 1);
+    
+        done();
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
       browser.fill('surname', 'Vespucci')
-      browser.pressButton('submit')
-      
-      done();
+      browser.pressButton('submit', () => {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.element('span#dates', 1);
+
+        done();
+      });
     });
   });
 });
